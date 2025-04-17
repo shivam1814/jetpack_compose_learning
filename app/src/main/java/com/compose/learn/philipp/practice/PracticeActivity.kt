f899771f788f17ff0ad3f7b461e6e51ac12df1bb
shivam <mandaliashivam72@gmail.com>
2025-04-17 18:50:44 +0530

    learn animation from Philipp commit by shivam

--- app/src/main/java/com/compose/learn/philipp/practice/PracticeActivity.kt
+++ app/src/main/java/com/compose/learn/philipp/practice/PracticeActivity.kt
@@ -1,10 +1,16 @@
 package com.compose.learn.philipp.practice
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.collection.scatterSetOf
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,10 +21,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -26,11 +31,17 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -40,9 +51,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
@@ -62,24 +74,145 @@ class PracticeActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ShowPreview() {
-    //1. all about Text
+    // all about Text
     //CustomTxt()
 
-    //2. all about state
+    // all about state
     //TwoColorBox()
 
-    //3. all about scaffold , textfield & snackbar
+    // all about scaffold , textfield & snackbar
     //SnackBar()
 
-    //4. all about list
+    // all about list
     //List()
 
-    //5. all about ConstraintLayout
-    Constraint()
+    // all about ConstraintLayout
+    //Constraint()
 
+    // animation
+    //SimpleAnimation()
+
+    // Circle Progress
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressBar(percentage = 0.8f, number = 100)
+    }
 
 
 }
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
+
+@Composable
+fun SimpleAnimation() {
+    var sizeState by remember { mutableStateOf(200.dp) }
+
+    val sizeAnimate by animateDpAsState(
+        targetValue = sizeState,
+        animationSpec = /*tween(
+            durationMillis = 2000,
+            delayMillis = 300,
+            easing = LinearOutSlowInEasing
+        )*/
+        /*spring(
+            Spring.DampingRatioLowBouncy
+        )*/
+        /*keyframes {
+            durationMillis = 5000
+            sizeState at 0 with LinearEasing
+            sizeState * 1.5f at 1000 with FastOutLinearInEasing
+            sizeState * 2f at 5000
+        }*/
+        tween(
+            durationMillis = 1000
+        )
+    )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .size(sizeAnimate)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Button(onClick = {
+                sizeState += 50.dp
+            }) {
+                Text(text = "Increase size")
+            }
+            Button(onClick = {
+                sizeState -= 50.dp
+            }) {
+                Text(text = "Decrease size")
+            }
+        }
+    }
+}
+
+@Composable
+fun EffectHandler() {
+    //side effect are something that escape scope of the compose function
+}
+
 
 @Composable
 fun Constraint() {
@@ -88,18 +221,19 @@ fun Constraint() {
         val redBox = createRefFor("redBox")
 
         constrain(greenBox) {
-            top.linkTo(parent.top)
             start.linkTo(parent.start)
-            width = Dimension.value(100.dp)
-            height = Dimension.value(100.dp)
-        }
-        constrain(redBox) {
             top.linkTo(parent.top)
-            start.linkTo(greenBox.end)
-            width = Dimension.value(100.dp)
             height = Dimension.value(100.dp)
+            width = Dimension.value(100.dp)
         }
-        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+
+        constrain(redBox) {
+            start.linkTo(greenBox.end)
+            top.linkTo(parent.top)
+            height = Dimension.value(100.dp)
+            width = Dimension.value(100.dp)
+        }
+
     }
     ConstraintLayout(constraint, modifier = Modifier.fillMaxSize()) {
         Box(
