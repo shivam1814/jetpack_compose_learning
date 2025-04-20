e39d8f263a605d64ed0194ff54f4ccd7d8429259
shivam <mandaliashivam72@gmail.com>
2025-04-20 22:18:07 +0530

    continue learning Meditation UI from philip in compose by shivam

--- app/src/main/java/com/compose/learn/philipp/meditationUi/ui/HomeScreen.kt
+++ app/src/main/java/com/compose/learn/philipp/meditationUi/ui/HomeScreen.kt
@@ -4,13 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,11 +30,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.OffsetEffect
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.learn.R
+import com.compose.learn.philipp.meditationUi.Feature
 import com.compose.learn.philipp.meditationUi.ui.theme.ButtonBlue
 import com.compose.learn.philipp.meditationUi.ui.theme.DarkerButtonBlue
 import com.compose.learn.philipp.meditationUi.ui.theme.DeepBlue
@@ -45,6 +56,8 @@ fun HomeScreen() {
         Column {
             GreetingSection()
             ChipSection(chip = listOf("Sweet sleep", "Insomnia", "Depression"))
+            CurrentMeditation()
+            FeatureSection()
         }
     }
 }
@@ -124,5 +137,104 @@ fun ChipSection(
 fun CurrentMeditation(
     color: Color = LightRed
 ) {
-
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column() {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White
+            )
+            Text(
+                text = "Meditation 3-10 min",
+                style = MaterialTheme.typography.bodyMedium,
+                color = TextWhite,
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_play),
+                contentDescription = "play",
+                tint = Color.White,
+                modifier = Modifier.size(14.dp)
+            )
+        }
+    }
 }
+
+
+@Composable
+fun FeatureSection(
+    feature: List<Feature>
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.headlineLarge,
+            color = TextWhite,
+            modifier = Modifier
+                .padding(15.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(feature.size) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        //medium color path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
