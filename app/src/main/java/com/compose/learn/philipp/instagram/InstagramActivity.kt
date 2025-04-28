e78d0ef8e5b1a8a95f4de17f3fe28fab69c670a1
shivam <mandaliashivam72@gmail.com>
2025-04-28 19:28:48 +0530

    start instagram ui from Philipp commit by shivam

---
+++ app/src/main/java/com/compose/learn/philipp/instagram/InstagramActivity.kt
@@ -0,0 +1,211 @@
+package com.compose.learn.philipp.instagram
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.compose.learn.R
+
+class InstagramActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContent {
+            Column(modifier = Modifier.fillMaxSize()) {
+                FunPreview()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FunPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(
+            "shviam_mandalia",
+            modifier = Modifier
+                .padding(top = 10.dp, bottom = 10.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
+    }
+}
+
+@Composable
+fun TopBar(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "back",
+            tint = Color.Black,
+            modifier = modifier.size(24.dp)
+        )
+        Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_bell),
+            contentDescription = "back",
+            tint = Color.Black,
+            modifier = modifier.size(24.dp)
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_dotmenu),
+            contentDescription = "back",
+            tint = Color.Black,
+            modifier = modifier.size(20.dp)
+        )
+    }
+}
+
+
+@Composable
+fun ProfileSection(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            RoundImage(
+                image = painterResource(R.drawable.philipp),
+                modifier = modifier
+                    .size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = modifier.width(16.dp))
+            StatSection(
+                modifier = modifier
+                    .weight(7f)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+
+@Composable
+fun StatSection(
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat("601", "Posts")
+        ProfileStat("100k", "Followers")
+        ProfileStat("71", "Following")
+    }
+
+}
+
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+        Spacer(modifier = modifier.height(4.dp))
+        Text(
+            text = text,
+        )
+    }
+}
+
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int,
+    modifier: Modifier = Modifier
+) {
+
+}
+
+
+
+
+
+
+
+
